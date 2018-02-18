4fb6aa3e9ec60c06e25b64376ddb3ad1707b05ab
Divyansh00 <divyanshchowdhary96@gmail.com>
2018-02-18 22:08:51 +0530

    Added Discussion Forum --Final Version

--- app/src/main/java/com/tutorials/a8086basics/Chat.java
+++ app/src/main/java/com/tutorials/a8086basics/Chat.java
@@ -1,13 +1,129 @@
 package com.tutorials.a8086basics;
 
-import android.app.Activity;
+
+import android.os.Build;
 import android.os.Bundle;
 
-public class Chat extends Activity {
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Chat extends AppCompatActivity implements View.OnClickListener,
+        MessageDataSource.MessagesCallbacks{
+
+    public static final String USER_EXTRA = "USER";
+
+    public static final String TAG = "ChatActivity";
+
+    private ArrayList<Message> mMessages;
+    private MessagesAdapter mAdapter;
+    private String mRecipient;
+    private ListView mListView;
+    private Date mLastMessageDate = new Date();
+    private String mConvoId;
+    private MessageDataSource.MessagesListener mListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mRecipient = "Expert Panel";
+
+        mListView = findViewById(R.id.messages_list);
+        mMessages = new ArrayList<>();
+        mAdapter = new MessagesAdapter(mMessages);
+        mListView.setAdapter(mAdapter);
+
+        setTitle(mRecipient);
+
+
+        Button sendMessage = (Button)findViewById(R.id.send_message);
+        sendMessage.setOnClickListener(this);
+
+        String[] ids = {"Student","-", "Expert"};
+        Arrays.sort(ids);
+        mConvoId = ids[0]+ids[1]+ids[2];
+
+        mListener = MessageDataSource.addMessagesListener(mConvoId, this);
+
+    }
+
+    public void onClick(View v) {
+        EditText newMessageView = (EditText)findViewById(R.id.new_message);
+        String newMessage = newMessageView.getText().toString();
+        newMessageView.setText("");
+        Message msg = new Message();
+        msg.setDate(new Date());
+        msg.setText(newMessage);
+        msg.setSender("Student");
+
+
+        MessageDataSource.saveMessage(msg, mConvoId);
+    }
+
+    @Override
+    public void onMessageAdded(Message message) {
+        mMessages.add(message);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MessageDataSource.stop(mListener);
+    }
+
+
+    private class MessagesAdapter extends ArrayAdapter<Message> {
+        MessagesAdapter(ArrayList<Message> messages){
+            super(Chat.this, R.layout.message_item, R.id.message, messages);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Message message = getItem(position);
+
+            TextView nameView = (TextView)convertView.findViewById(R.id.message);
+
+                nameView.setText(message.getText());
+
+                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) nameView.getLayoutParams();
+
+                int sdk = Build.VERSION.SDK_INT;
+                if (message.getSender().equals("Student")) {
+                    if (sdk >= Build.VERSION_CODES.JELLY_BEAN) {
+                        nameView.setBackground(getDrawable(R.drawable.bubble_right_green));
+                    } else {
+                        nameView.setBackgroundDrawable(getDrawable(R.drawable.bubble_right_green));
+                    }
+                    layoutParams.gravity = Gravity.RIGHT;
+                } else {
+                    if (sdk >= Build.VERSION_CODES.JELLY_BEAN) {
+                        nameView.setBackground(getDrawable(R.drawable.bubble_left_gray));
+                    } else {
+                        nameView.setBackgroundDrawable(getDrawable(R.drawable.bubble_left_gray));
+                    }
+                    layoutParams.gravity = Gravity.LEFT;
+                }
+
+                nameView.setLayoutParams(layoutParams);
+
+
+            return convertView;
+
+
+        }
     }
 }
--- app/src/main/java/com/tutorials/a8086basics/Message.java
+++ app/src/main/java/com/tutorials/a8086basics/Message.java
@@ -1,8 +1,37 @@
 package com.tutorials.a8086basics;
 
+import java.util.Date;
+
 /**
  * Created by Divyansh on 18-02-2018.
  */
 
 public class Message {
+    private String mText;
+    private String mSender;
+    private Date mDate;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public String getSender() {
+        return mSender;
+    }
+
+    public void setSender(String sender) {
+        mSender = sender;
+    }
 }
